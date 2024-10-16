a3cfb7ce05caed6b5a41eaeef25469b46d60888a
hyeonWoo <glass1726@naver.com>
2024-10-16 19:33:36 +0900

    Add: 메인 콘텐츠 섹션 추가

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
 import './App.css';
 import Header from './components/Header';
+import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-// import UserPage from './pages/UserPage';
-// import Test from './pages/Test';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
-
 function App() {
   return (
-
     <Router>
       <div className="App">
         <Header />
-
-        {/* 
-        <main>
-          <Routes>
-            <Route path="/" element={<h2>환영합니다!</h2>} /> 
-        <Route path="/user" element={<UserPage />} /> 
-        <Route path="/test" element={<Test />} />
-      </Routes>
-        </main>
-
-    */}
-
+        <MainContent />
         <Footer />
       </div>
     </Router>
-
   );
 }
 
